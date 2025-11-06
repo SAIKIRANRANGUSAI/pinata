637b31ec560fb64920755446c9b60592e19442b6
SAIKIRANRANGUSAI <saikiranrangu6@gmail.com>
2025-11-06 10:22:13 +0530

    added

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,16 @@ import * as express from 'express';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  // 👇 Tell Nest we’re using Express
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Static files (CSS, JS, etc.)
+  // Serve static assets (CSS, images, JS)
   app.use(express.static(join(__dirname, '..', 'public')));
 
-  // EJS view engine setup
+  // EJS view engine
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`🚀 Server running at http://localhost:3000`);
 }
 bootstrap();
