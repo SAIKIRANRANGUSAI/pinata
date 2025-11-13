4820032b8b023bcb7eceb3658a1c18e4b8bd585c
SAIKIRANRANGUSAI <saikiranrangu6@gmail.com>
2025-11-13 19:05:40 +0530

    s

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,12 @@ import * as hbs from 'hbs';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Static files
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  // Views
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  app.engine('hbs', hbs.__express);
 
   await app.listen(process.env.PORT || 3000);
-  console.log(`Server running`);
 }
 
 bootstrap();
